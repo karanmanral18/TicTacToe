7c489cb04852f2dfc0c919aaf57fbd551985cb0e
KaranManral <40225914+KaranManral@users.noreply.github.com>
2020-04-22 14:03:38 +0530

    Updates

---
+++ precache-manifest.020962e8c50d4aa94194c6fe7384043c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4b7614212fe28a079f65d9590d4408b",
+    "url": "/react-TicTacToe/index.html"
+  },
+  {
+    "revision": "f194fadbb591c9a59093",
+    "url": "/react-TicTacToe/static/css/main.f915b639.chunk.css"
+  },
+  {
+    "revision": "584afb3c2325a08b4be7",
+    "url": "/react-TicTacToe/static/js/2.93c6d887.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-TicTacToe/static/js/2.93c6d887.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f194fadbb591c9a59093",
+    "url": "/react-TicTacToe/static/js/main.bb8179b5.chunk.js"
+  },
+  {
+    "revision": "32e278b3d1e2fbfb2ec4",
+    "url": "/react-TicTacToe/static/js/runtime-main.77cb67fa.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-tic-tac-toe/precache-manifest.aa2aa3878d8809d24b137ebad169d02c.js"
+  "/react-TicTacToe/precache-manifest.020962e8c50d4aa94194c6fe7384043c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-tic-tac-toe/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-TicTacToe/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
