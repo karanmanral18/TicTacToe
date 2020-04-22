ebd8e7caa64190f04055a2b2d40f8b31c90eb0f4
KaranManral <40225914+KaranManral@users.noreply.github.com>
2020-04-22 14:19:32 +0530

    Updates

---
+++ precache-manifest.ff2b173919f68ab5fabacf53861480bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7616ff217f144279ed40726311c6fb7",
+    "url": "/TicTacToe/index.html"
+  },
+  {
+    "revision": "df0f4214e73f28ac82de",
+    "url": "/TicTacToe/static/css/main.9c64b251.chunk.css"
+  },
+  {
+    "revision": "584afb3c2325a08b4be7",
+    "url": "/TicTacToe/static/js/2.93c6d887.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TicTacToe/static/js/2.93c6d887.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df0f4214e73f28ac82de",
+    "url": "/TicTacToe/static/js/main.becb8f44.chunk.js"
+  },
+  {
+    "revision": "446111ee2df19d3a9e25",
+    "url": "/TicTacToe/static/js/runtime-main.115da5eb.js"
+  }
+]);
